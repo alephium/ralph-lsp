2cc4d1a0e1cd65b416baab212b9c4ff216cc3afa
Thomas Droxler <thomas.droxler@gmail.com>
2024-01-29 16:12:38 +0100

    Configure jar location through settings
    If jar isn't found, an error message is displayed and a button redirect
    to the setting

--- plugin-vscode/src/extension.ts
+++ plugin-vscode/src/extension.ts
@@ -1,4 +1,6 @@
 import {ExtensionContext, workspace} from 'vscode';
+import * as vscode from 'vscode'
+import * as fs from 'fs'
 
 import {LanguageClient, LanguageClientOptions, ServerOptions} from 'vscode-languageclient/node';
 
@@ -7,30 +9,38 @@ let client: LanguageClient;
 export function activate(context: ExtensionContext) {
     console.log("Activating Ralph LSP client");
 
-    const args: string[] = ["-jar", "-DRALPH_LSP_HOME=<path-to-your-log-folder>", "<path-to-your-jar>/ralph-lsp.jar"];
+    const config =  workspace.getConfiguration('ralphLSP')
+    const jarPath = config.get('server.jar') as string
 
-    const serverOptions: ServerOptions = {
-        command: "java",
-        args: args
-    };
+    if(fs.existsSync(jarPath)) {
+        const args: string[] = ["-jar", "-DRALPH_LSP_HOME=<path-to-your-log-folder>", jarPath]
+        const serverOptions: ServerOptions = {
+            command: "java",
+            args: args
+        };
 
-    const clientOptions: LanguageClientOptions = {
-        documentSelector: [
-            {pattern: '**/*.ral'},
-            {language: 'json', pattern: '**/ralph.json'},
-        ]
-    };
+        const clientOptions: LanguageClientOptions = {
+            documentSelector: [
+                {pattern: '**/*.ral'},
+                {language: 'json', pattern: '**/ralph.json'},
+            ]
+        };
 
-    // Create the client and store it.
-    client = new LanguageClient(
-        'ralph-lsp',
-        'Ralph LSP',
-        serverOptions,
-        clientOptions
-    );
+        // Create the client and store it.
+        client = new LanguageClient(
+            'ralph-lsp',
+            'Ralph LSP',
+            serverOptions,
+            clientOptions
+        );
 
-    // Start the client.
-    client.start();
+        // Start the client.
+        client.start();
+    } else {
+        vscode.window
+        .showErrorMessage(`${jarPath} doesn't exist` , 'Open setting')
+        .then(_ => { vscode.commands.executeCommand("workbench.action.openSettings", "ralphLSP.server.jar")})
+    }
 }
 
 export function deactivate(): Thenable<void> | undefined {
