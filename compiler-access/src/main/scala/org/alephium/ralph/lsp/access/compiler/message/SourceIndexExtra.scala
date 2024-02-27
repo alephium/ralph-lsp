47f6264c9f7b90df6607e1a8cd2d4a3bd5eb2e14
simerplaha <simer.j@gmail.com>
2024-02-27 14:43:35 +1100

    remove `SourceIndexTypeExtension`

--- compiler-access/src/main/scala/org/alephium/ralph/lsp/access/compiler/message/SourceIndexExtra.scala
+++ compiler-access/src/main/scala/org/alephium/ralph/lsp/access/compiler/message/SourceIndexExtra.scala
@@ -7,6 +7,23 @@ object SourceIndexExtra {
   val zero: SourceIndex =
     org.alephium.ralph.SourceIndex.empty
 
+  /**
+   * Sending negative index to the client would be incorrect.
+   * This set the index to be an empty range.
+   *
+   * This is a temporary solution until an AST is available for `ralph.json`.
+   *
+   * @see Issue <a href="https://github.com/alephium/ralph-lsp/issues/17">#17</a>.
+   */
+  def ensurePositive(index: Int, width: Int): SourceIndex =
+    if (index < 0)
+      zero
+    else
+      new SourceIndex(
+        index = index,
+        width = width
+      )
+
   implicit class SourceIndexExtension(val sourceIndex: SourceIndex) extends AnyVal {
     def from: Int =
       sourceIndex.index
@@ -24,25 +41,5 @@ object SourceIndexExtra {
     /** Offset this SourceIndex */
     def +(right: Int): SourceIndex =
       sourceIndex.copy(index = from + right)
-  }
-
-  implicit class SourceIndexTypeExtension(val sourceIndex: SourceIndex.type) extends AnyVal {
-
-    /**
-     * Sending negative index to the client would be incorrect.
-     * This set the index to be an empty range.
-     *
-     * This is a temporary solution until an AST is available for `ralph.json`.
-     *
-     * @see Issue <a href="https://github.com/alephium/ralph-lsp/issues/17">#17</a>.
-     */
-    def ensurePositive(index: Int, width: Int): SourceIndex =
-      if (index < 0)
-        zero
-      else
-        new SourceIndex(
-          index = index,
-          width = width
-        )
   }
 }
--- presentation-compiler/src/main/scala/org/alephium/ralph/lsp/pc/workspace/build/Build.scala
+++ presentation-compiler/src/main/scala/org/alephium/ralph/lsp/pc/workspace/build/Build.scala
@@ -3,7 +3,6 @@ package org.alephium.ralph.lsp.pc.workspace.build
 import org.alephium.ralph.SourceIndex
 import org.alephium.ralph.lsp.access.compiler.CompilerAccess
 import org.alephium.ralph.lsp.access.compiler.message.SourceIndexExtra
-import org.alephium.ralph.lsp.access.compiler.message.SourceIndexExtra._
 import org.alephium.ralph.lsp.access.file.FileAccess
 import org.alephium.ralph.lsp.pc.log.ClientLogger
 import org.alephium.ralph.lsp.pc.workspace.build.BuildState._
@@ -260,13 +259,13 @@ object Build {
     val artifactPath = parsed.config.artifactPath
 
     val contractPathIndex =
-      SourceIndex.ensurePositive(
+      SourceIndexExtra.ensurePositive(
         index = parsed.code.lastIndexOf(contractPath), // TODO: lastIndexOf is temporary solution until an AST is available.
         width = contractPath.length
       )
 
     val artifactPathIndex =
-      SourceIndex.ensurePositive(
+      SourceIndexExtra.ensurePositive(
         index = parsed.code.lastIndexOf(artifactPath), // TODO: lastIndexOf is temporary solution until an AST is available.
         width = artifactPath.length
       )
@@ -283,7 +282,7 @@ object Build {
     val errorIndexToken =
       parsed.config.dependencyPath getOrElse "}"
 
-    SourceIndex.ensurePositive(
+    SourceIndexExtra.ensurePositive(
       index = parsed.code.lastIndexOf(errorIndexToken), // TODO: lastIndexOf is temporary solution until an AST is available.
       width = errorIndexToken.length
     )
