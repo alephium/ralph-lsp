73d5cfe2f343cc6310803b8b6e427eee8c323fe3
simerplaha <simer.j@gmail.com>
2024-02-29 16:40:23 +1100

    tests `NodeWalkParentsSpec`

--- compiler-access/src/test/scala/org/alephium/ralph/lsp/access/compiler/ast/NodeWalkParentsSpec.scala
+++ compiler-access/src/test/scala/org/alephium/ralph/lsp/access/compiler/ast/NodeWalkParentsSpec.scala
@@ -1,5 +1,6 @@
 package org.alephium.ralph.lsp.access.compiler.ast
 
+import org.scalatest.OptionValues._
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
@@ -28,6 +29,53 @@ class NodeWalkParentsSpec extends AnyWordSpec with Matchers {
       val lastChild = root.children.last
       lastChild.data shouldBe "Child-2"
       lastChild.walkParents.map(_.data).toList should contain only "Parent-1"
+    }
+  }
+
+  "return non-empty" when {
+    "1-3-2-2-1 and 1-3-2-2-2 (middle nodes)" in {
+      val midNode1 =
+        TestNode
+          .root
+          .walkDown
+          .find(_.data == "1-3-2-2-1")
+          .value
+
+      val midNode2 =
+        TestNode
+          .root
+          .walkDown
+          .find(_.data == "1-3-2-2-2")
+          .value
+
+      // Note: midNode1 and midNode2 are siblings so their parents must be the same
+      val expectedParents =
+        List(
+          "1-3-2-2",
+          "1-3-2",
+          "1-3",
+          "1"
+        )
+
+      Array(midNode1, midNode2) foreach {
+        node =>
+          node.walkParents.map(_.data).toList shouldBe expectedParents
+      }
+    }
+
+    "1-4-2 (bottom last node)" in {
+      val lastNode =
+        TestNode
+          .root
+          .walkDown
+          .find(_.data == "1-4-2")
+          .value
+
+      lastNode.walkParents.map(_.data).toList shouldBe
+        List(
+          "1-4",
+          "1"
+        )
     }
   }
 }
