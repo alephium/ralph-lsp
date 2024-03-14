705585b663210f849b7fb7325aba5e19e4a078d7
simerplaha <simer.j@gmail.com>
2024-03-14 18:28:27 +1100

    removed `Scope` and `ScopeBuilder`

--- presentation-compiler/src/main/scala/org/alephium/ralph/lsp/pc/search/gotodef/GoToIdent.scala
+++ presentation-compiler/src/main/scala/org/alephium/ralph/lsp/pc/search/gotodef/GoToIdent.scala
@@ -1,11 +1,9 @@
 package org.alephium.ralph.lsp.pc.search.gotodef
 
 import org.alephium.ralph.Ast
-import org.alephium.ralph.Ast.Positioned
 import org.alephium.ralph.lsp.access.compiler.ast.Tree
 import org.alephium.ralph.lsp.access.compiler.ast.node.Node
 import org.alephium.ralph.lsp.access.compiler.message.SourceIndexExtra.SourceIndexExtension
-import org.alephium.ralph.lsp.pc.search.scope.ScopeBuilder
 
 import scala.collection.immutable.ArraySeq
 
@@ -19,7 +17,7 @@ private object GoToIdent {
    * @param source    The source tree to search within.
    * @return An array sequence of positioned ASTs matching the search result.
    * */
-  def goTo(identNode: Node[Positioned],
+  def goTo(identNode: Node[Ast.Positioned],
            ident: Ast.Ident,
            source: Tree.Source): ArraySeq[Ast.Positioned] =
     identNode
@@ -28,8 +26,8 @@ private object GoToIdent {
       .collect {
         case Node(variable: Ast.Variable[_], _) if variable.id == ident => // Is it a variable?
           // The user clicked on a variable. Take 'em there!
-          val variables =
-            goToVariable(
+          val arguments =
+            goToArguments(
               variableNode = identNode,
               variable = variable,
               source = source
@@ -41,7 +39,7 @@ private object GoToIdent {
               ident = ident
             )
 
-          variables ++ constants
+          arguments ++ constants
 
         case Node(fieldSelector: Ast.EnumFieldSelector[_], _) if fieldSelector.field == ident =>
           // The user clicked on an enum field. Take 'em there!
@@ -72,7 +70,7 @@ private object GoToIdent {
           // The user clicked on an constant definition. Take 'em there!
           goToVariableUsages(
             ident = constantDef.ident,
-            source = source
+            from = source.rootNode
           )
       }
       .flatten
@@ -85,23 +83,23 @@ private object GoToIdent {
    * @param source       The source tree to search within.
    * @return An array sequence of [[Ast.Argument]]s matching the search result.
    * */
-  private def goToVariable(variableNode: Node[Positioned],
-                           variable: Ast.Variable[_],
-                           source: Tree.Source): ArraySeq[Ast.Argument] =
-    variable
-      .sourceIndex
-      .to(ArraySeq)
-      .flatMap {
-        variableIndex =>
-          ScopeBuilder
-            .buildArguments(source)
-            .filter {
-              argument =>
-                argument.typeDef.ident == variable.id &&
-                  argument.scope.contains(variableIndex.index)
-            }
-            .map(_.typeDef)
-      }
+  private def goToArguments(variableNode: Node[Ast.Positioned],
+                            variable: Ast.Variable[_],
+                            source: Tree.Source): Iterator[Ast.Argument] = {
+    val functionArguments =
+      goToNearestFunctionArguments(
+        childNode = variableNode,
+        ident = variable.id
+      )
+
+    val templateArguments =
+      goToTemplateArguments(
+        source = source,
+        ident = variable.id
+      )
+
+    functionArguments ++ templateArguments
+  }
 
   /**
    * Navigate to the constant definitions for the given identifier.
@@ -164,16 +162,15 @@ private object GoToIdent {
       .to(ArraySeq)
 
   /**
-   * Navigate to all variable usages for the given identifier.
+   * Navigate to all variable usages for the given variable identifier.
    *
-   * @param ident  The variable identifier.
-   * @param source The source tree to search within.
+   * @param ident The variable identifier to search for.
+   * @param from  The node to search within, walking downwards.
    * @return An array sequence of variable usage IDs.
    */
   private def goToVariableUsages(ident: Ast.Ident,
-                                 source: Tree.Source): ArraySeq[Ast.Ident] =
-    source
-      .rootNode
+                                 from: Node[Ast.Positioned]): ArraySeq[Ast.Ident] =
+    from
       .walkDown
       .collect {
         // find all the selections matching the variable name.
@@ -181,4 +178,68 @@ private object GoToIdent {
           variable.id
       }
       .to(ArraySeq)
+
+  /**
+   * Navigate to the nearest function definition for which the given child node is in scope.
+   *
+   * @param childNode The node to traverse up from.
+   * @return An Option containing the nearest function definition, if found.
+   */
+  private def goToNearestFuncDef(childNode: Node[Ast.Positioned]): Option[Ast.FuncDef[_]] =
+    childNode
+      .data
+      .sourceIndex
+      .flatMap {
+        childNodeIndex =>
+          childNode
+            .walkParents
+            .collectFirst {
+              case Node(function: Ast.FuncDef[_], _) if function.sourceIndex.exists(_ contains childNodeIndex.index) =>
+                function
+            }
+      }
+
+  /**
+   * Navigate to the argument(s) of the nearest function to this node.
+   *
+   * @param childNode The node to traverse up in search of the function.
+   * @param ident     The variable identifier to find arguments for.
+   * @return An array sequence of [[Ast.Argument]]s matching the search result.
+   * */
+  private def goToNearestFunctionArguments(childNode: Node[Ast.Positioned],
+                                           ident: Ast.Ident): Iterator[Ast.Argument] =
+    goToNearestFuncDef(childNode)
+      .iterator
+      .flatMap(_.args.filter(_.ident == ident))
+
+  /**
+   * Navigate to the template argument(s) for the given identifier.
+   *
+   * @param source The source tree to search within.
+   * @param ident  The variable identifier to find arguments for.
+   * @return An array sequence of [[Ast.Argument]]s matching the search result.
+   * */
+  private def goToTemplateArguments(source: Tree.Source,
+                                    ident: Ast.Ident): Seq[Ast.Argument] = {
+    val arguments =
+      source.ast match {
+        case Left(contract) =>
+          contract match {
+            case ast: Ast.TxScript =>
+              ast.templateVars
+
+            case contract: Ast.Contract =>
+              contract.templateVars ++ contract.fields
+
+            case _: Ast.ContractInterface =>
+              Seq.empty
+          }
+
+        case Right(_) =>
+          Seq.empty
+      }
+
+    arguments.filter(_.ident == ident)
+  }
+
 }
