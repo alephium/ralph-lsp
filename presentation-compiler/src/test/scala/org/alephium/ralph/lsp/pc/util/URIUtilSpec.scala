c6d314e2acceb2b7f093896bfe9ccf39f99e9380
Thomas Droxler <thomas.droxler@gmail.com>
2024-03-15 08:54:20 +0100

    Clean uris from escaped characters when receiving client messages

---
+++ presentation-compiler/src/test/scala/org/alephium/ralph/lsp/pc/util/URIUtilSpec.scala
@@ -0,0 +1,23 @@
+package org.alephium.ralph.lsp.pc.util
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import java.net.URI
+import java.net.URLDecoder.decode
+
+class URIUtilSpec extends AnyWordSpec with Matchers {
+
+  "URIUtil" should {
+    "build and clean uri" when {
+      "Windows uri contains escaped characters" in {
+        if (scala.util.Properties.isWin) {
+          val uri = "file:///c%3A/Users/user/test.ral"
+          val expected = "file:///c:/Users/user/test.ral"
+
+          URIUtil.uri(uri) shouldBe new URI(expected )
+        }
+      }
+    }
+  }
+}
