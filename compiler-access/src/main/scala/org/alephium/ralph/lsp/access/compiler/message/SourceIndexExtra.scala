a5e16b7c9f27d4ec8ec656c56ef35236018edf12
Thomas Droxler <thomas.droxler@gmail.com>
2024-03-20 07:41:00 +0100

    Move `buildLineRange` to `StringUtil`

--- compiler-access/src/main/scala/org/alephium/ralph/lsp/access/compiler/message/SourceIndexExtra.scala
+++ compiler-access/src/main/scala/org/alephium/ralph/lsp/access/compiler/message/SourceIndexExtra.scala
@@ -2,7 +2,7 @@ package org.alephium.ralph.lsp.access.compiler.message
 
 import fastparse.IndexedParserInput
 import org.alephium.ralph.{SourceIndex, SourcePosition}
-import org.alephium.ralph.lsp.access.util.StringUtil._
+import org.alephium.ralph.lsp.access.util.StringUtil
 
 import java.net.URI
 
@@ -55,81 +55,7 @@ object SourceIndexExtra {
 
     /** Convert [[SourceIndex]] that contains index information to [[LineRange]] that contains line and character information */
     def toLineRange(code: String): LineRange = {
-      buildLineRange(code, sourceIndex.from, sourceIndex.to)
-    }
-  }
-
-  /**
-   * Constructs a line range from the provided source code, using the specified start and end indexes.
-   * The while loop optimizes performance by stopping once the necessary indexes are found.
-   *
-   * Note: There's a special case to handle where a line ending can be either '\r' or '\r\n'.
-   *
-   * In the latter case, we need to avoid returning on '\r' alone, as '\n' completes the line ending.
-   * Therefore, we always check the next character.
-   *
-   * If the 'to' index corresponds to the last character of the code, the loop terminates
-   * after finding the end index without attempting to access code(i + 1), preventing an IndexOutOfBoundsException.
-   *
-   * @param code     The source code
-   * @param from     The start index
-   * @param to       The end index
-   */
-
-  def buildLineRange(code: String, from: Int, to: Int): LineRange = {
-    if(from < 0 || to < 0 || to < from || from >= code.length || to >= code.length || code.length <= 1) {
-      LineRange.zero
-    } else {
-      var line = 0
-      var col = 0
-      var index = 0
-      var char: Character = null
-      var next: Character = null
-
-      var start: LinePosition = null
-      var end: LinePosition = null
-
-      @inline def newLine = {
-        line += 1
-        col = 0
-        index+=1
-        char = next
-      }
-      @inline def nextChar = {
-        col += 1
-        index+=1
-        char = next
-      }
-
-      char = code(index)
-      while (index < code.length && end == null) {
-        if (index == from && start == null) {
-          // Start index found
-          start = LinePosition(line, col)
-        }
-        if (index == to && end == null) {
-          // End index found, we stop the loop here
-          end = LinePosition(line, col)
-        } else {
-          // We look one char forward to differentiate `\r\n` and `\r` line endings
-          next = code(index + 1)
-          if (char == '\r' && next == '\n') {
-            nextChar
-          } else if (char == '\r' && next != '\n') {
-            newLine
-          } else if (char == '\n') {
-            newLine
-          } else {
-            nextChar
-          }
-        }
-      }
-
-      if(start == null || end == null){
-        LineRange.zero
-      } else {
-        LineRange(start, end)
-      }
+      StringUtil.buildLineRange(code, sourceIndex.from, sourceIndex.to)
     }
   }
 }
--- compiler-access/src/test/scala/org/alephium/ralph/lsp/access/StringUtilSpec.scala
+++ compiler-access/src/test/scala/org/alephium/ralph/lsp/access/StringUtilSpec.scala
@@ -3,8 +3,7 @@ package org.alephium.ralph.lsp.access.util
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.alephium.ralph.lsp.access.compiler.message._
-import org.alephium.ralph.lsp.access.util.StringUtil.computeIndex
-
+import org.alephium.ralph.lsp.access.util.StringUtil._
 
 class StringUtilSpec extends AnyWordSpec with Matchers {
 
@@ -29,6 +28,55 @@ class StringUtilSpec extends AnyWordSpec with Matchers {
         computeIndex("line1\nline2\rline3\r\nline4@")
         computeIndex("line1\nline2\rline3\r\n@line4")
         computeIndex("line1\n\r\r\n\r@line2")
+    }
+  }
+
+  def testLineRange(code: String): Unit = {
+    val lines = code.split("\n|\r\n|\r").zipWithIndex
+
+    val start = lines.filter(_._1.contains(">>"))
+    val end = lines.filter(_._1.contains("<<"))
+
+    val from = code.indexOf(">>")
+    val to = code.indexOf("<<") - 3
+
+    val expectedLineRange =
+      start
+        .zip(end)
+        .map {
+          case ((startLine, startLineIndex), (endLine, endLineIndex)) =>
+            LineRange(
+              from = LinePosition(startLineIndex, startLine.indexOf(">>")),
+              to = LinePosition(endLineIndex, endLine.replaceFirst(">>", "").indexOf("<<") - 1)
+            )
+        }.head
+
+    val codeWithoutSymbol =
+      code.replaceAll(">>|<<", "")
+
+    buildLineRange(codeWithoutSymbol,from, to) shouldBe expectedLineRange
+  }
+
+  "SourceIndexExtra" should {
+    "build line range" in {
+      testLineRange(">>l<<ine1\nline2\r\nline3\rline4")
+      testLineRange(">>line1<<\nline2\r\nline3\rline4")
+      testLineRange("line1\nli>>n<<e2\r\nline3\rline4")
+      testLineRange("line1\nline2\r\nline3\r>>line4<<")
+      testLineRange(">>line1\nline2\r\nli<<ne3\rline4")
+      testLineRange(">>line1\nline2\r\nline3\rline4<<")
+      testLineRange("line1\n>>line2\r\nline3\rline4<<")
+      testLineRange("line1\nline2\r\n>>line3\rline4<<")
+    }
+
+    "fail to build time range with invalid arguments" in {
+      val code  = "line1\nline2\r\nline3\rline4"
+      buildLineRange(code, 0, code.length) shouldBe LineRange.zero
+      buildLineRange(code, 1, 0) shouldBe LineRange.zero
+      buildLineRange(code, -1, 1) shouldBe LineRange.zero
+      buildLineRange(code, 1, -1) shouldBe LineRange.zero
+      buildLineRange(code, code.length, code.length) shouldBe LineRange.zero
+      buildLineRange(code, code.length+1, code.length+1) shouldBe LineRange.zero
     }
   }
 }
