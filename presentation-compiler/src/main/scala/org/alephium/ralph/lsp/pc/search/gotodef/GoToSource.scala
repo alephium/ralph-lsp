fcd882d1b7e97b5079ee17945b5ba7b79a160d60
simerplaha <simer.j@gmail.com>
2024-03-26 16:11:34 +1100

    remove unnecessary `Iterator` conversion from `GoToSource`.

--- presentation-compiler/src/main/scala/org/alephium/ralph/lsp/pc/search/gotodef/GoToSource.scala
+++ presentation-compiler/src/main/scala/org/alephium/ralph/lsp/pc/search/gotodef/GoToSource.scala
@@ -31,7 +31,7 @@ private object GoToSource {
     // covert go-to node to GoToLocation
     GoToLocation(
       sourceCode = sourceCode,
-      ast = goToResult
+      ast = goToResult.to(ArraySeq)
     )
   }
 
@@ -40,39 +40,43 @@ private object GoToSource {
    *
    * @param cursorIndex The index of the token clicked by the user.
    * @param source      The source tree to navigate within.
-   * @return An option containing the positioned AST element if found, otherwise None.
+   * @return An iterator over the positioned AST elements found.
    */
   private def goTo(cursorIndex: Int,
-                   source: Tree.Source): ArraySeq[Ast.Positioned] =
-    source
-      .rootNode
-      .findLast(_.sourceIndex.exists(_ contains cursorIndex)) // find the node closest to this source-index
-      .to(ArraySeq)
-      .collect {
-        case identNode @ Node(ident: Ast.Ident, _) =>
-          // the clicked/closest node is an ident
-          GoToIdent.goTo(
-            identNode = identNode,
-            ident = ident,
-            source = source
-          )
+                   source: Tree.Source): Iterator[Ast.Positioned] =
+    source.rootNode.findLast(_.sourceIndex.exists(_ contains cursorIndex)) match { // find the node closest to this source-index
+      case Some(closest) =>
+        closest match {
+          case identNode @ Node(ident: Ast.Ident, _) =>
+            // the clicked/closest node is an ident
+            GoToIdent.goTo(
+              identNode = identNode,
+              ident = ident,
+              source = source
+            )
 
-        case funcIdNode @ Node(funcId: Ast.FuncId, _) =>
-          // the clicked/closest node is functionId
-          GoToFuncId.goTo(
-            funcIdNode = funcIdNode,
-            funcId = funcId,
-            source = source
-          )
+          case funcIdNode @ Node(funcId: Ast.FuncId, _) =>
+            // the clicked/closest node is functionId
+            GoToFuncId.goTo(
+              funcIdNode = funcIdNode,
+              funcId = funcId,
+              source = source
+            )
 
-        case typIdNode @ Node(typeId: Ast.TypeId, _) =>
-          // the clicked/closest node is TypeId
-          GoToTypeId.goTo(
-            identNode = typIdNode,
-            typeId = typeId,
-            source = source
-          )
-      }
-      .flatten
+          case typIdNode @ Node(typeId: Ast.TypeId, _) =>
+            // the clicked/closest node is TypeId
+            GoToTypeId.goTo(
+              identNode = typIdNode,
+              typeId = typeId,
+              source = source
+            )
+
+          case _ =>
+            Iterator.empty
+        }
+
+      case None =>
+        Iterator.empty
+    }
 
 }
