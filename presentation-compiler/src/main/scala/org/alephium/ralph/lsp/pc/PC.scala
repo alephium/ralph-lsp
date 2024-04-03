ab9dda5b1cdbe122ddaab9b6cecf05b7b087420e
simerplaha <simer.j@gmail.com>
2024-04-03 12:20:40 +1100

    documentation

--- presentation-compiler/src/main/scala/org/alephium/ralph/lsp/pc/PC.scala
+++ presentation-compiler/src/main/scala/org/alephium/ralph/lsp/pc/PC.scala
@@ -5,16 +5,23 @@ import org.alephium.ralph.lsp.access.file.FileAccess
 import org.alephium.ralph.lsp.pc.log.ClientLogger
 import org.alephium.ralph.lsp.pc.util.URIUtil
 import org.alephium.ralph.lsp.pc.workspace.build.error.ErrorUnknownFileType
-import org.alephium.ralph.lsp.pc.workspace.{Workspace, WorkspaceFile, WorkspaceFileEvent, WorkspaceState}
 import org.alephium.ralph.lsp.pc.workspace.build.{Build, BuildState}
+import org.alephium.ralph.lsp.pc.workspace.{Workspace, WorkspaceFile, WorkspaceFileEvent, WorkspaceState}
 
 import java.net.URI
 import scala.collection.immutable.ArraySeq
 
+/**
+ * Implements public presentation compiler APIs.
+ *
+ * To invoke a compilation, there are only two ways:
+ *  - When a file is changed, use [[PC.changed]]
+ *  - When a file is deleted or created, use [[PC.deleteOrCreate]]
+ */
 object PC {
 
   /**
-   * Initialises a new presentation compiler.
+   * Initialises a new presentation compiler state.
    *
    * @param workspaceURI The [[URI]] of the directory where the workspace is initialised.
    * @return A new [[PCState]] instance representing the compiler state with no compilation.
@@ -25,7 +32,16 @@ object PC {
       buildErrors = None
     )
 
-  /** Process source or build file change for a workspace that may or may not be source-aware ([[WorkspaceState.IsSourceAware]]) */
+  /**
+   * Processes a change in source or build file for a workspace that may or may not be source-aware ([[WorkspaceState.IsSourceAware]]).
+   *
+   * @param fileURI The URI of the changed file.
+   * @param code    An optional string containing the code of the changed file.
+   *                If this is [[None]], the file's content will fetched from disk.
+   * @param pcState The current state of the presentation compiler.
+   * @return Either an [[ErrorUnknownFileType]] if the file type is unknown,
+   *         or an optional new [[PCState]] instance representing the updated compiler state.
+   */
   def changed(fileURI: URI,
               code: Option[String],
               pcState: PCState)(implicit file: FileAccess,
@@ -90,12 +106,12 @@ object PC {
     }
 
   /**
-   * Delete or create a file within the workspace that may or may not be source-aware ([[WorkspaceState.IsSourceAware]])
+   * Deletes or creates a file within the workspace, which may or may not be source-aware ([[WorkspaceState.IsSourceAware]]).
    *
-   * @param events  Events to process
-   * @param pcState Current workspace and build state
-   * @return Workspace change result
-   * */
+   * @param events  Events to process.
+   * @param pcState Current presentation compiler state.
+   * @return The updated presentation compiler state containing the compilation result.
+   */
   def deleteOrCreate(events: ArraySeq[WorkspaceFileEvent],
                      pcState: PCState)(implicit file: FileAccess,
                                        compiler: CompilerAccess,
@@ -156,7 +172,13 @@ object PC {
         )
     }
 
-  /** Apply build change to the [[PCState]] */
+  /**
+   * Applies a build change to the [[PCState]].
+   *
+   * @param buildChangeResult The result of the build change operation.
+   * @param pcState           The current presentation compiler state.
+   * @return The updated presentation compiler state.
+   */
   private def buildChanged(buildChangeResult: Either[BuildState.BuildErrored, WorkspaceState],
                            pcState: PCState): PCState =
     buildChangeResult match {
@@ -179,7 +201,13 @@ object PC {
         )
     }
 
-  /** Apply source-code change to the [[PCState]] */
+  /**
+   * Applies a source code change to the [[PCState]].
+   *
+   * @param sourceChangeResult The result of the source code change operation.
+   * @param pcState            The current presentation compiler state.
+   * @return The updated presentation compiler state.
+   */
   private def sourceCodeChanged(sourceChangeResult: Either[BuildState.BuildErrored, WorkspaceState],
                                 pcState: PCState): PCState =
     sourceChangeResult match {
--- presentation-compiler/src/main/scala/org/alephium/ralph/lsp/pc/PCStateDiagnostics.scala
+++ presentation-compiler/src/main/scala/org/alephium/ralph/lsp/pc/PCStateDiagnostics.scala
@@ -15,9 +15,11 @@ object PCStateDiagnostics {
 
   /**
    * Given the current [[PCState]] and the next [[PCState]],
-   * return new diagnostics to publish.
+   * returns new diagnostics to publish.
    *
-   * @return Diagnostics clearing resolved diagnostics dispatched in previous state.
+   * @param currentState The current presentation compiler state.
+   * @param newState     The next presentation compiler state.
+   * @return An iterator over resolved diagnostics dispatched in the previous state and new diagnostics.
    */
   def toFileDiagnostics(currentState: PCState,
                         newState: PCState): Iterable[FileDiagnostic] = {
