e987a18b7d6d04a507a49c947ea42c6ae90b6c41
simerplaha <simer.j@gmail.com>
2024-05-15 13:57:27 +1000

    remove `BuiltIn.contains`

--- presentation-compiler/src/main/scala/org/alephium/ralph/lsp/pc/search/completion/FunctionBodyCompleter.scala
+++ presentation-compiler/src/main/scala/org/alephium/ralph/lsp/pc/search/completion/FunctionBodyCompleter.scala
@@ -128,7 +128,10 @@ object FunctionBodyCompleter {
 
         case Node(function: Ast.FuncDef[_], _) =>
           // suggest function names
-          Suggestion.FuncDef(SourceLocation.Node(function, sourceCode))
+          Suggestion.FuncDef(
+            node = SourceLocation.Node(function, sourceCode),
+            isBuiltIn = false
+          )
 
         case Node(eventDef: Ast.EventDef, _) =>
           // suggest events
@@ -155,19 +158,28 @@ object FunctionBodyCompleter {
         WorkspaceSearcher
           .collectTrees(builtIn)
           .iterator
-          .flatMap(suggestsFunctions)
+          .flatMap {
+            source =>
+              suggestFunctions(
+                source = source,
+                isBuiltInSource = true
+              )
+          }
 
       case None =>
         Iterator.empty
     }
 
   /**
-   * Suggests all functions available from this source code.
+   * Suggests all functions available in the given source code.
    *
-   * @param source The source code that may contain functions.
-   * @return An iterator over suggested functions.
+   * @param source          The source code that may contain function definitions.
+   * @param isBuiltInSource True if the given source is from the built-in dependency, false otherwise.
+   * @return An iterator over the suggested functions.
    */
-  private def suggestsFunctions(source: SourceLocation.Code): Iterator[Suggestion.FuncDef] =
+  private def suggestFunctions(
+      source: SourceLocation.Code,
+      isBuiltInSource: Boolean): Iterator[Suggestion.FuncDef] =
     source.tree.ast match {
       case Left(contract) =>
         contract
@@ -181,7 +193,10 @@ object FunctionBodyCompleter {
                   source = source
                 )
 
-              Suggestion.FuncDef(node)
+              Suggestion.FuncDef(
+                node = node,
+                isBuiltIn = isBuiltInSource
+              )
           }
 
       case Right(_) =>
--- presentation-compiler/src/main/scala/org/alephium/ralph/lsp/pc/workspace/build/dependency/DependencyID.scala
+++ presentation-compiler/src/main/scala/org/alephium/ralph/lsp/pc/workspace/build/dependency/DependencyID.scala
@@ -16,8 +16,6 @@
 
 package org.alephium.ralph.lsp.pc.workspace.build.dependency
 
-import java.net.URI
-import java.nio.file.{Paths, Path}
 import scala.collection.immutable.ArraySeq
 
 sealed trait DependencyID extends Product {
@@ -29,29 +27,8 @@ sealed trait DependencyID extends Product {
 
 object DependencyID {
 
-  case object Std extends DependencyID
-
-  case object BuiltIn extends DependencyID {
-
-    /**
-     * Checks if the built-in dependency contains the given file.
-     *
-     * @param fileURI The URI of the file to check.
-     * @return True if the file is contained in the built-in dependency, otherwise false.
-     */
-    def contains(fileURI: URI): Boolean =
-      contains(Paths.get(fileURI))
-
-    /**
-     * Checks if the built-in dependency contains the given file.
-     *
-     * @param filePath The Path of the file to check.
-     * @return True if the file is contained in the built-in dependency, otherwise false.
-     */
-    def contains(filePath: Path): Boolean =
-      filePath.getParent.getFileName.toString == BuiltIn.dirName
-
-  }
+  case object Std     extends DependencyID
+  case object BuiltIn extends DependencyID
 
   def all(): ArraySeq[DependencyID] =
     ArraySeq(
