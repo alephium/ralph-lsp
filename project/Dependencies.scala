1d3b5c827b0539f0b67cb1ab9c13cf8e4b14135a
simerplaha <simer.j@gmail.com>
2024-06-04 16:06:22 +1000

    bump `ralphc`

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,7 +15,7 @@ object Dependencies {
   lazy val scalaMock  = "org.scalamock"     %% "scalamock"       % "6.0.0"    % Test
 
   /** Core */
-  lazy val ralphc = "org.alephium" %% "alephium-ralphc" % "3.0.1" excludeAll (
+  lazy val ralphc = "org.alephium" %% "alephium-ralphc" % "3.0.2" excludeAll (
     ExclusionRule(organization = "org.rocksdb"),
     ExclusionRule(organization = "io.prometheus"),
     ExclusionRule(organization = "org.alephium", name = "alephium-api_2.13"),
