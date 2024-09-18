b37d51f8ff0a531047e1ebcf0a400eaba7fbe496
simerplaha <simer.j@gmail.com>
2024-09-18 10:54:01 +1000

    moved `goToNearestFuncDef` to `GoToRefIdent`

--- presentation-compiler/src/main/scala/org/alephium/ralph/lsp/pc/search/gotoref/GoToRefIdent.scala
+++ presentation-compiler/src/main/scala/org/alephium/ralph/lsp/pc/search/gotoref/GoToRefIdent.scala
@@ -19,8 +19,8 @@ package org.alephium.ralph.lsp.pc.search.gotoref
 import org.alephium.ralph.Ast
 import org.alephium.ralph.lsp.access.compiler.ast.Tree
 import org.alephium.ralph.lsp.access.compiler.ast.node.Node
+import org.alephium.ralph.lsp.access.compiler.message.SourceIndexExtra.SourceIndexExtension
 import org.alephium.ralph.lsp.pc.log.{ClientLogger, StrictImplicitLogging}
-import org.alephium.ralph.lsp.pc.search.gotoref.GoToRefTypeId.goToNearestFuncDef
 import org.alephium.ralph.lsp.pc.sourcecode.SourceLocation
 import org.alephium.ralph.lsp.pc.workspace.{WorkspaceState, WorkspaceSearcher}
 
@@ -505,5 +505,33 @@ private object GoToRefIdent extends StrictImplicitLogging {
         case Node(variable: Ast.AssignmentTarget[_], _) if variable.ident == ident =>
           SourceLocation.Node(variable.ident, sourceCode)
       }
+
+  /**
+   * Navigate to the nearest function definition for which the given child node is in scope.
+   *
+   * @param childNode The node to traverse up from.
+   * @return An Option containing the nearest function definition, if found.
+   */
+  private def goToNearestFuncDef(childNode: Node[Ast.Positioned, Ast.Positioned]): Option[Node[Ast.FuncDef[_], Ast.Positioned]] =
+    childNode.data match {
+      case function: Ast.FuncDef[_] =>
+        // Nested function definitions are not allowed in Ralph.
+        // If the input node is a function, return the node itself.
+        Some(childNode.upcast(function))
+
+      case ast: Ast.Positioned =>
+        // For everything else, find the nearest function.
+        ast
+          .sourceIndex
+          .flatMap {
+            childNodeIndex =>
+              childNode
+                .walkParents
+                .collectFirst {
+                  case node @ Node(function: Ast.FuncDef[_], _) if function.sourceIndex.exists(_ contains childNodeIndex.index) =>
+                    node.upcast(function)
+                }
+          }
+    }
 
 }
--- presentation-compiler/src/main/scala/org/alephium/ralph/lsp/pc/search/gotoref/GoToRefTypeId.scala
+++ presentation-compiler/src/main/scala/org/alephium/ralph/lsp/pc/search/gotoref/GoToRefTypeId.scala
@@ -19,7 +19,6 @@ package org.alephium.ralph.lsp.pc.search.gotoref
 import org.alephium.ralph.Ast
 import org.alephium.ralph.lsp.access.compiler.ast.AstExtra
 import org.alephium.ralph.lsp.access.compiler.ast.node.Node
-import org.alephium.ralph.lsp.access.compiler.message.SourceIndexExtra.SourceIndexExtension
 import org.alephium.ralph.lsp.pc.log.{ClientLogger, StrictImplicitLogging}
 import org.alephium.ralph.lsp.pc.sourcecode.SourceLocation
 import org.alephium.ralph.lsp.pc.workspace.{WorkspaceState, WorkspaceSearcher}
@@ -121,34 +120,6 @@ private object GoToRefTypeId extends StrictImplicitLogging {
       case None =>
         logger.error(s"Parent node not found for AST '${definition.data.getClass.getSimpleName}' at source index '${definition.data.sourceIndex}'")
         Iterator.empty
-    }
-
-  /**
-   * Navigate to the nearest function definition for which the given child node is in scope.
-   *
-   * @param childNode The node to traverse up from.
-   * @return An Option containing the nearest function definition, if found.
-   */
-  def goToNearestFuncDef(childNode: Node[Ast.Positioned, Ast.Positioned]): Option[Node[Ast.FuncDef[_], Ast.Positioned]] =
-    childNode.data match {
-      case function: Ast.FuncDef[_] =>
-        // Nested function definitions are not allowed in Ralph.
-        // If the input node is a function, return the node itself.
-        Some(childNode.upcast(function))
-
-      case ast: Ast.Positioned =>
-        // For everything else, find the nearest function.
-        ast
-          .sourceIndex
-          .flatMap {
-            childNodeIndex =>
-              childNode
-                .walkParents
-                .collectFirst {
-                  case node @ Node(function: Ast.FuncDef[_], _) if function.sourceIndex.exists(_ contains childNodeIndex.index) =>
-                    node.upcast(function)
-                }
-          }
     }
 
   /**
