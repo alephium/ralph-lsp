21e7e59814c185ca65efc012f35bfb6132fca890
simerplaha <simer.j@gmail.com>
2024-10-01 10:54:19 +1000

    #267: Warnings with `SourceIndex`

--- compiler-access/src/main/scala/org/alephium/ralph/lsp/access/compiler/message/warning/StringWarning.scala
+++ compiler-access/src/main/scala/org/alephium/ralph/lsp/access/compiler/message/warning/StringWarning.scala
@@ -16,6 +16,7 @@
 
 package org.alephium.ralph.lsp.access.compiler.message.warning
 
+import org.alephium.ralph
 import org.alephium.ralph.SourceIndex
 import org.alephium.ralph.lsp.access.compiler.message.{CompilerMessage, SourceIndexExtra}
 
@@ -33,6 +34,46 @@ object StringWarning {
       message = message,
       index = SourceIndexExtra.zero(fileURI)
     )
+
+  /**
+   * Creates an instance of [[StringWarning]] with the specified file [[URI]].
+   *
+   * @param warning [[ralph.Warning]] to convert into a [[StringWarning]].
+   * @param fileURI File [[URI]] where this warning is to be reported.
+   * @return A [[StringWarning]] with the specified file [[URI]] set.
+   */
+  @inline def apply(
+      warning: ralph.Warning,
+      fileURI: URI): StringWarning =
+    warning.sourceIndex match {
+      case Some(sourceIndex) =>
+        sourceIndex.fileURI match {
+          case Some(sourceIndexFileURI) => // SourceIndex contains a fileURI.
+            // TODO: Resolved in next PR: Replace this as it throws Exceptions.
+            //       It should be replaced with error message logged using ClientLogger.
+            // There should never be a case where fileURI are different.
+            // This assert is performed to detect bugs; Warnings should always be mapped to the right fileURI.
+            assert(sourceIndexFileURI == fileURI, s"Warning's '$fileURI' is not equal to SourceFile's URI '$sourceIndexFileURI'")
+
+            StringWarning(
+              message = warning.message,
+              index = sourceIndex
+            )
+
+          case None =>
+            // fileURI is not provided in SourceIndex, use the one provided as input.
+            StringWarning(
+              message = warning.message,
+              index = sourceIndex.copy(fileURI = Some(fileURI))
+            )
+        }
+
+      case None =>
+        StringWarning(
+          message = warning.message,
+          fileURI = fileURI
+        )
+    }
 
 }
 
