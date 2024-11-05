4a49a2cca5d6869bc51101f383c88dcb85829d3a
simerplaha <simer.j@gmail.com>
2024-11-06 09:19:55 +1100

    Added `LogMessage` for typed messages

--- utils/src/main/scala/org/alephium/ralph/lsp/utils/log/LogMessage.scala
+++ utils/src/main/scala/org/alephium/ralph/lsp/utils/log/LogMessage.scala
@@ -16,27 +16,9 @@
 
 package org.alephium.ralph.lsp.utils.log
 
-import com.typesafe.scalalogging.Logger
+/** Type log message */
+trait LogMessage {
 
-/** Defines APIs for publishing logs to an LSP client */
-trait ClientLogger {
-
-  def info(message: String)(implicit logger: Logger): Unit
-
-  def warning(message: String)(implicit logger: Logger): Unit
-
-  def error(message: String)(implicit logger: Logger): Unit
-
-  def error(message: LogMessage)(implicit logger: Logger): Unit =
-    this.error(message.message)
-
-  def error(
-      message: String,
-      cause: Throwable
-    )(implicit logger: Logger): Unit
-
-  def debug(message: String)(implicit logger: Logger): Unit
-
-  def trace(message: String)(implicit logger: Logger): Unit
+  def message: String
 
 }
