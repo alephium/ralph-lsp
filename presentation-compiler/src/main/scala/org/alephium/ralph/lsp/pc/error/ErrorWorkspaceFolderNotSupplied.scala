2b7f0f9abf2225d8fdb46c2a807c6a2ac81a7cda
simerplaha <simer.j@gmail.com>
2025-03-20 10:45:43 +1100

    fix package name

--- presentation-compiler/src/main/scala/org/alephium/ralph/lsp/pc/error/ErrorWorkspaceFolderNotSupplied.scala
+++ presentation-compiler/src/main/scala/org/alephium/ralph/lsp/pc/error/ErrorWorkspaceFolderNotSupplied.scala
@@ -1,7 +1,7 @@
 // Copyright (c) Alephium
 // SPDX-License-Identifier: LGPL-3.0-only
 
-package org.alephium.ralph.lsp.pc.notification
+package org.alephium.ralph.lsp.pc.error
 
 import org.alephium.ralph.lsp.access.compiler.message.CompilerMessage
 import org.alephium.ralph.SourceIndex
