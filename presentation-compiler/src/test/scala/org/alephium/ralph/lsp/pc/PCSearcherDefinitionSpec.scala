de928a223668900eec6c7c1d041dc48f54fe6902
simerplaha <simer.j@gmail.com>
2025-03-20 16:40:31 +1100

    improve function names

--- presentation-compiler/src/test/scala/org/alephium/ralph/lsp/pc/PCSearcherDefinitionSpec.scala
+++ presentation-compiler/src/test/scala/org/alephium/ralph/lsp/pc/PCSearcherDefinitionSpec.scala
@@ -14,6 +14,7 @@ import org.alephium.ralph.lsp.pc.workspace.{TestWorkspace, WorkspaceState}
 import org.alephium.ralph.lsp.utils.IsCancelled
 import org.alephium.ralph.Ast
 import org.alephium.ralph.lsp.access.util.TestCodeUtil
+import org.alephium.ralph.lsp.utils.log.ClientLogger
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.OptionValues._
@@ -27,82 +28,17 @@ import scala.concurrent.duration._
 
 class PCSearcherDefinitionSpec extends AnyWordSpec with Matchers with ScalaFutures {
 
-  implicit val compiler: CompilerAccess      = CompilerAccess.ralphc
-  implicit val fileAccess: FileAccess        = FileAccess.disk
-  implicit val logger: TestClientLogger.type = TestClientLogger
-  implicit val ec: ExecutionContext          = ExecutionContext.Implicits.global
+  implicit val compiler: CompilerAccess = CompilerAccess.ralphc
+  implicit val fileAccess: FileAccess   = FileAccess.disk
+  implicit val logger: ClientLogger     = TestClientLogger
+  implicit val ec: ExecutionContext     = ExecutionContext.Implicits.global
 
   "search only the strict-ast" when {
-    def runTest(enableSoftParser: Boolean): (Either[CompilerMessage.Error, ArraySeq[GoToDef]], WorkspaceState.IsParsed) = {
-      // Create a workspace with:
-      // - one source file `Child` that has valid syntax, i.e. it's both soft and strict parseable
-      // - one source file `Parent` that has invalid syntax, i.e. it's only soft parseable
-      val workspace =
-        TestWorkspace
-          .genParsed(
-            // `Child` has valid syntax and is strict and soft parseable.
-            """
-              |Contract Child() extends Parent() {
-              |  fn function() -> () {
-              |    let call = function()
-              |  }
-              |}
-              |""".stripMargin,
-            // `Parent` is only soft parseable.
-            """
-              |Contract Parent {
-              |  fn function()
-              |}
-              |""".stripMargin
-          )
-          .sample
-          .value
-
-      // the workspace contains two source-files
-      workspace.sourceCode should have size 2
-
-      // both source files are code-aware
-      val sourceFiles =
-        workspace.sourceCode.map(_.asInstanceOf[SourceCodeState.IsCodeAware])
-
-      // location to click
-      val click =
-        TestCodeUtil.lineRange(
-          """
-            |Contract Child() extends Parent() {
-            |  fn function() -> () {
-            |    let call = functi>>o<<n()
-            |  }
-            |}
-            |""".stripMargin
-        )
-
-      val pcStates =
-        TestPCStates.genPCStates(workspace)
-
-      val result =
-        PCSearcher
-          .definition(
-            fileURI = sourceFiles.head.fileURI,
-            line = click.from.line,
-            character = click.from.character,
-            enableSoftParser = enableSoftParser,
-            // format: off
-            isCancelled = IsCancelled(() => false), // format: on
-            pcStates = pcStates
-          )
-          .futureValue(Timeout(5.seconds))
-
-      TestWorkspace delete workspace
-
-      (result, workspace)
-    }
-
     "soft-parser is disabled" in {
       // When soft-parse is disabled, search results are only returned from the `Child` contract
       // which has 100% valid syntax.
       val (result, workspace) =
-        runTest(enableSoftParser = false)
+        createWorkspaceAndRunGoToDef(enableSoftParser = false)
 
       val locations =
         result
@@ -124,12 +60,14 @@ class PCSearcherDefinitionSpec extends AnyWordSpec with Matchers with ScalaFutur
 
       sourceIndexes should contain only expectedIndex
     }
+  }
 
+  "search both strict & Soft ASTs" when {
     "soft-parser is enabled" in {
       // When soft parsing is enabled, search results are returned for both `Parent` and `Child` contracts.
       // Even though `Parent` has syntax errors, it is soft-parsable.
       val (result, _) =
-        runTest(enableSoftParser = true)
+        createWorkspaceAndRunGoToDef(enableSoftParser = true)
 
       val actualIndexes =
         result
@@ -158,6 +96,71 @@ class PCSearcherDefinitionSpec extends AnyWordSpec with Matchers with ScalaFutur
 
       actualIndexes should contain only (expectedIndex1, expectedIndex2)
     }
+  }
+
+  private def createWorkspaceAndRunGoToDef(enableSoftParser: Boolean): (Either[CompilerMessage.Error, ArraySeq[GoToDef]], WorkspaceState.IsParsed) = {
+    // Create a workspace with:
+    // - one source file `Child` that has valid syntax, i.e. it's both soft and strict parseable
+    // - one source file `Parent` that has invalid syntax, i.e. it's only soft parseable
+    val workspace =
+      TestWorkspace
+        .genParsed(
+          // `Child` has valid syntax and is strict and soft parseable.
+          """
+            |Contract Child() extends Parent() {
+            |  fn function() -> () {
+            |    let call = function()
+            |  }
+            |}
+            |""".stripMargin,
+          // `Parent` is only soft parseable.
+          """
+            |Contract Parent {
+            |  fn function()
+            |}
+            |""".stripMargin
+        )
+        .sample
+        .value
+
+    // the workspace contains two source-files
+    workspace.sourceCode should have size 2
+
+    // both source files are code-aware
+    val sourceFiles =
+      workspace.sourceCode.map(_.asInstanceOf[SourceCodeState.IsCodeAware])
+
+    // location to click
+    val click =
+      TestCodeUtil.lineRange(
+        """
+          |Contract Child() extends Parent() {
+          |  fn function() -> () {
+          |    let call = functi>>o<<n()
+          |  }
+          |}
+          |""".stripMargin
+      )
+
+    val pcStates =
+      TestPCStates.genPCStates(workspace)
+
+    val result =
+      PCSearcher
+        .definition(
+          fileURI = sourceFiles.head.fileURI,
+          line = click.from.line,
+          character = click.from.character,
+          enableSoftParser = enableSoftParser,
+          // format: off
+          isCancelled = IsCancelled(() => false), // format: on
+          pcStates = pcStates
+        )
+        .futureValue(Timeout(5.seconds))
+
+    TestWorkspace delete workspace
+
+    (result, workspace)
   }
 
 }
