19052289a78fd94d7d27444566fdb93269a60c91
simerplaha <simer.j@gmail.com>
2025-06-04 10:49:53 +1000

    use named parameters

--- presentation-compiler/src/test/scala/org/alephium/ralph/lsp/pc/workspace/build/typescript/TestTSBuildFile.scala
+++ presentation-compiler/src/test/scala/org/alephium/ralph/lsp/pc/workspace/build/typescript/TestTSBuildFile.scala
@@ -17,16 +17,18 @@ object TestTSBuildFile {
       ignoreUpdateFieldsCheckWarnings      <- Gen.option(arbitrary[Boolean])
       ignoreCheckExternalCallerWarnings    <- Gen.option(arbitrary[Boolean])
       ignoreUnusedFunctionReturnWarnings   <- Gen.option(arbitrary[Boolean])
+      skipAbstractContractCheck            <- Gen.option(arbitrary[Boolean])
       errorOnWarnings                      <- Gen.option(arbitrary[Boolean])
     } yield TSConfig.CompilerOptions(
-      ignoreUnusedConstantsWarnings,
-      ignoreUnusedVariablesWarnings,
-      ignoreUnusedFieldsWarnings,
-      ignoreUnusedPrivateFunctionsWarnings,
-      ignoreUpdateFieldsCheckWarnings,
-      ignoreCheckExternalCallerWarnings,
-      ignoreUnusedFunctionReturnWarnings,
-      errorOnWarnings
+      ignoreUnusedConstantsWarnings = ignoreUnusedConstantsWarnings,
+      ignoreUnusedVariablesWarnings = ignoreUnusedVariablesWarnings,
+      ignoreUnusedFieldsWarnings = ignoreUnusedFieldsWarnings,
+      ignoreUnusedPrivateFunctionsWarnings = ignoreUnusedPrivateFunctionsWarnings,
+      ignoreUpdateFieldsCheckWarnings = ignoreUpdateFieldsCheckWarnings,
+      ignoreCheckExternalCallerWarnings = ignoreCheckExternalCallerWarnings,
+      ignoreUnusedFunctionReturnWarnings = ignoreUnusedFunctionReturnWarnings,
+      skipAbstractContractCheck = skipAbstractContractCheck,
+      errorOnWarnings = errorOnWarnings
     )
 
   def genTSConfig: Gen[TSConfig] =
