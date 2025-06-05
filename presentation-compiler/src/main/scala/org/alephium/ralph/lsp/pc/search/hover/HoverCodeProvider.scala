a609aa3572113a097486a4bff33d4085e7c8d2b4
Thomas Droxler <thomas.droxler@gmail.com>
2025-06-05 17:00:32 +0200

    Move hover function declaration to `HoverDeclaration`

--- presentation-compiler/src/main/scala/org/alephium/ralph/lsp/pc/search/hover/HoverCodeProvider.scala
+++ presentation-compiler/src/main/scala/org/alephium/ralph/lsp/pc/search/hover/HoverCodeProvider.scala
@@ -3,7 +3,6 @@
 
 package org.alephium.ralph.lsp.pc.search.hover
 
-import org.alephium.ralph.SourceIndex
 import org.alephium.ralph.lsp.access.compiler.parser.soft.ast.SoftAST
 import org.alephium.ralph.lsp.pc.search.CodeProvider
 import org.alephium.ralph.lsp.pc.search.gotodef._
@@ -60,69 +59,16 @@ private[search] case object HoverCodeProvider extends CodeProvider[SourceCodeSta
                 case Some(definition) =>
                   definition
                     .walkParents
-                    .collectFirst { // Find the nearest parent declaration.
+                    .collectFirst { // Find the nearest parent.
                       case Node(declaration: SoftAST.DeclarationAST, _) =>
-                        declaration
+                        HoverDeclaration(declaration, sourceCode)
                     }
-                    .flatMap {
-                      declaration =>
-                        buildHover(declaration, sourceCode)
-                    }
+                    .flatten
 
                 case None =>
                   None
               }
           }
     }
-
-  def buildHover(
-      declaration: SoftAST.DeclarationAST,
-      sourceCode: SourceCodeState.IsParsed): Option[SourceLocation.Hover] =
-    buildHoverContent(declaration).map {
-      case (content, index) =>
-        SourceLocation.Hover(
-          content = content,
-          contentPosition = index,
-          parsed = sourceCode
-        )
-    }
-
-  /**
-   * Builds the hover content for a given AST declaration.
-   *
-   * This function checks the type of the declaration and builds the content accordingly.
-   *
-   * @param declaration The declaration to build the hover content for.
-   * @return An optional tuple containing the content and its source index.
-   */
-  def buildHoverContent(declaration: SoftAST.DeclarationAST): Option[(String, SourceIndex)] =
-    declaration match {
-      case function: SoftAST.Function => Some(buildFunctionContent(function))
-      case _: SoftAST.Template        => None
-      case _: SoftAST.Enum            => None
-      case _: SoftAST.Struct          => None
-      case _: SoftAST.Event           => None
-      case _: SoftAST.Const           => None
-    }
-
-  /**
-   * Builds the content for a function declaration.
-   *
-   * Blocks and post-signature spaces are not included in the content,
-   * so their width is subtracted from the function's index width.
-   *
-   * @param function The function declaration to build the content for.
-   * @return A tuple containing the function declaration code and its source index.
-   */
-  private def buildFunctionContent(function: SoftAST.Function): (String, SourceIndex) = {
-
-    val blockIndexWidth              = function.block.map(_.index.width).getOrElse(0)
-    val postSignatureSpaceIndexWidth = function.postSignatureSpace.map(_.index.width).getOrElse(0)
-
-    val index = function.index.copy(width = function.index.width - postSignatureSpaceIndexWidth - blockIndexWidth)
-
-    (function.copy(block = None).toCode(), index)
-
-  }
 
 }
