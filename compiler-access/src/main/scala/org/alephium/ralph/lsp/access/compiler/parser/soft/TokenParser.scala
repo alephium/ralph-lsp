9d000e04039c83c921bcf93bdfde8f21ea9ec42a
simerplaha <simer.j@gmail.com>
2025-07-18 15:15:49 +1000

    Check for comments in `InfixOperatorOrFail`

--- compiler-access/src/main/scala/org/alephium/ralph/lsp/access/compiler/parser/soft/TokenParser.scala
+++ compiler-access/src/main/scala/org/alephium/ralph/lsp/access/compiler/parser/soft/TokenParser.scala
@@ -29,12 +29,44 @@ private object TokenParser {
     }
 
   def parseOrFail[Unknown: P, T <: Token](token: T): P[SoftAST.TokenDocumented[T]] =
-    P(Index ~ CommentParser.parseOrFail.? ~ CodeParser.parseOrFail(token) ~ Index) map {
+    P {
+      Index ~
+        CommentParser.parseOrFail.? ~
+        CodeParser.parseOrFail(token) ~
+        Index
+    } map {
       case (from, documentation, token, to) =>
         SoftAST.TokenDocumented(
           index = range(from, to),
           documentation = documentation,
           code = token
+        )
+    }
+
+  /**
+   * Parses the first successful token from the given iterator of tokens, failing if none succeed.
+   *
+   * @param prefixCheck If `true`, performs the prefix check before parsing each token.
+   * @param tokens      The tokens to parse.
+   */
+  def parseOrFailOneOf[Unknown: P, T <: Token](
+      prefixCheck: Boolean,
+      tokens: Iterator[T]): P[SoftAST.TokenDocumented[T]] =
+    P {
+      Index ~
+        CommentParser.parseOrFail.? ~
+        Index ~
+        ParserUtil.orTokenCombinator(prefixCheck = prefixCheck, tokens = tokens) ~
+        Index
+    } map {
+      case (commentFrom, documentation, tokenFrom, token, to) =>
+        SoftAST.TokenDocumented(
+          index = range(commentFrom, to),
+          documentation = documentation,
+          code = SoftAST.CodeToken(
+            index = range(tokenFrom, to),
+            token = token
+          )
         )
     }
 
@@ -53,10 +85,12 @@ private object TokenParser {
    * Parses all tokens of type [[Token.InfixOperator]] and also their comments.
    */
   def InfixOperatorOrFail[Unknown: P]: P[SoftAST.TokenDocumented[Token.InfixOperator]] =
-    ParserUtil.orCombinator(
-      items = Token.infix.iterator,
-      parser = TokenParser.parseOrFail(_: Token.InfixOperator)
-    )
+    P {
+      parseOrFailOneOf(
+        prefixCheck = false,
+        tokens = Token.infix.iterator
+      )
+    }
 
   /**
    * Reads characters until at least one of the input tokens is matched.
--- compiler-access/src/test/scala/org/alephium/ralph/lsp/access/compiler/parser/soft/InfixCallParserSpec.scala
+++ compiler-access/src/test/scala/org/alephium/ralph/lsp/access/compiler/parser/soft/InfixCallParserSpec.scala
@@ -6,6 +6,7 @@ package org.alephium.ralph.lsp.access.compiler.parser.soft
 import org.alephium.ralph.lsp.access.compiler.parser.soft.TestParser._
 import org.alephium.ralph.lsp.access.compiler.parser.soft.ast.SoftAST
 import org.alephium.ralph.lsp.access.compiler.parser.soft.ast.TestSoftAST._
+import org.alephium.ralph.lsp.access.util.TestCodeUtil._
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
@@ -24,6 +25,107 @@ class InfixCallParserSpec extends AnyWordSpec with Matchers {
 
       infix.operator shouldBe LessThanOrEqual("(one + one) >><=<< (this - that)")
     }
+  }
+
+  "comment exists before infix operator" in {
+    val infix =
+      parseInfixCall(
+        """one
+          |// comment
+          |+ one""".stripMargin
+      )
+
+    infix shouldBe
+      SoftAST.InfixExpression(
+        index = indexOf(
+          """>>one
+            |// comment
+            |+ one<<""".stripMargin
+        ),
+        leftExpression = Identifier(
+          """>>one<<
+            |// comment
+            |+ one""".stripMargin
+        ),
+        preOperatorSpace = Some(
+          Space(
+            """one>>
+              |<<// comment
+              |+ one""".stripMargin
+          )
+        ),
+        operator = SoftAST.TokenDocumented(
+          index = indexOf(
+            """one
+              |>>// comment
+              |+<< one""".stripMargin
+          ),
+          documentation = Some(
+            SoftAST.Comments(
+              index = indexOf(
+                """one
+                  |>>// comment
+                  |<<+ one""".stripMargin
+              ),
+              preCommentSpace = None,
+              comments = Seq(
+                SoftAST.Comment(
+                  index = indexOf(
+                    """one
+                      |>>// comment<<
+                      |+ one""".stripMargin
+                  ),
+                  doubleForwardSlash = DoubleForwardSlash(
+                    """one
+                      |>>//<< comment
+                      |+ one""".stripMargin
+                  ),
+                  preTextSpace = Some(
+                    Space(
+                      """one
+                      |//>> <<comment
+                      |+ one""".stripMargin
+                    )
+                  ),
+                  text = Some(
+                    CodeString(
+                      """one
+                      |// >>comment<<
+                      |+ one""".stripMargin
+                    )
+                  ),
+                  postTextSpace = None
+                )
+              ),
+              postCommentSpace = Some(
+                Space(
+                  """one
+                    |// comment>>
+                    |<<+ one""".stripMargin
+                )
+              )
+            )
+          ),
+          code = Plus(
+            """one
+                |// comment
+                |>>+<< one""".stripMargin
+          ).code
+        ),
+        postOperatorSpace = Some(
+          Space(
+            """one
+            |// comment
+            |+>> <<one""".stripMargin
+          )
+        ),
+        rightExpression = Identifier(
+          """one
+              |// comment
+              |+ >>one<<""".stripMargin
+        )
+      )
+
   }
 
 }
